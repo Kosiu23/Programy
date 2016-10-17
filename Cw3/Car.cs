6fc664d2b3f906e914b9e19c2ad05ff09f464bbc
Kosiu <kosiarz15@gmail.com>
2016-10-17 13:29:33 +0200

    3sws

---
+++ Cw3/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw3
+{
+    class Car
+    {
+        private double pojemonoscSilnika;              //2
+        private string marka;                          //2
+
+       public Car()                                   //4 i 8
+        {
+            this.marka = "Skoda";
+            this.pojemonoscSilnika = 5.1;            //4
+        }
+         public Car (double pojemnoscSilnika, string marka) //5 i 8
+        {
+            this.marka = marka; // skoda pobierze domyslnie
+            this.pojemonoscSilnika = pojemnoscSilnika; // 5.1 pobierze domyslnie
+        }
+        /* public void PokazMarke()
+         {
+             Console.WriteLine(marka);
+         }*/
+        static public Car Create()  //9
+         {
+             return new Car();  
+         }
+        static int iloscKol;  //11
+        static Car()
+        {
+            iloscKol = 4;
+        }
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamięć");
+            Console.WriteLine(this.marka);
+            Console.WriteLine(this.pojemonoscSilnika);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cw3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hermetyzacja wiedziec co to
+
+            Car car1 = new Car();       //3
+           // car1.pojemnoscSilnika = 5.1; //3 
+            /*car1.PokazMarke();*/
+            Car car2 = new Car(4.8, "Ford");
+            Car car3 = Car.Create();  //9
+            Console.ReadKey();
+        } 
+    }
+}
